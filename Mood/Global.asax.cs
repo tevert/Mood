d2ebc9306371613fa49f9fd26921c147ece21c1c
Tyler Evert <tevert2@gmail.com>
2016-12-22 00:37:07 -0600

    Adding config/code to allow running of migrations on app start for prod

--- Mood/Global.asax.cs
+++ Mood/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System.Web;
-using System.Web.Http;
+﻿using Mood.Migrations;
+using System.Configuration;
+using System.Data.Entity.Migrations;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +16,13 @@ namespace Mood
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // TODO move this somewhere sensible.
+            if (bool.Parse(ConfigurationManager.AppSettings["MigrateDatabaseToLatestVersion"]))
+            {
+                var migrator = new DbMigrator(new DbMigrationsConfiguration<ApplicationDBContext>());
+                migrator.Update();
+            }
         }
     }
 }
