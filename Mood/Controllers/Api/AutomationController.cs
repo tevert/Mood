c0fc6c296a1b5dd40b852b042db15daa857d9650
Tyler Evert <tevert2@gmail.com>
2017-01-16 18:08:00 -0600

    Adding SendGrid for mail service, and Swagger to expose the WebAPI to Azure Logic Apps

---
+++ Mood/Controllers/Api/AutomationController.cs
@@ -0,0 +1,24 @@
+﻿using Mood.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Mood.Controllers.Api
+{
+    public class AutomationController : ApiController
+    {
+        private IEmailService emailService;
+
+        public AutomationController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        // POST api/<controller>
+        public async Task<dynamic> Post()
+        {
+            return await emailService.SendEmailAsync("test@example.com", "CONTENT");
+        }
+    }
+}
---
+++ Mood/Services/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace Mood.Services
+{
+    public class EmailService : IEmailService
+    {
+        private string apiKey;
+        private string systemEmail;
+
+        public EmailService(string apiKey, string systemEmail)
+        {
+            this.apiKey = apiKey;
+            this.systemEmail = systemEmail;
+        }
+
+        public async Task<dynamic> SendEmailAsync(string recipient, string content)
+        {
+            dynamic sg = new SendGridAPIClient(apiKey); // SendGrid's client is a giant tree of dynamics, wtf
+
+            Email from = new Email(systemEmail);
+            string subject = "Sending with SendGrid is Fun";
+            Email to = new Email(recipient);
+            Content contentBlock = new Content("text/plain", content);
+            Mail mail = new Mail(from, subject, to, contentBlock);
+
+            return await sg.client.mail.send.post(requestBody: mail.Get());
+        }
+    }
+}
