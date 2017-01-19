fedb14506d1ec570e14844b29fa13f0b4664dffb
tevert <tevert2@gmail.com>
2017-01-19 13:52:04 -0600

    Refactoring JS dependency build process. NPM pulls, gulp pulls in, project includes, but we don't check any in.

--- Mood/App_Start/BundleConfig.cs
+++ Mood/App_Start/BundleConfig.cs
@@ -7,13 +7,13 @@ namespace Mood
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            var jsDepRoot = "~/Scripts/node_modules";
-            var cssDepRoot = "~/Content/node_modules";
+            var jsDepRoot = "~/Scripts/vendor";
+            var cssDepRoot = "~/Content/vendor";
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                $"{jsDepRoot}/jquery/dist/jquery.js"));
+                $"{jsDepRoot}/jquery/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                $"{jsDepRoot}/knockout/build/output/knockout-latest.js"));
+                $"{jsDepRoot}/knockout/knockout-latest.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/surveyApp").Include(
                 "~/Scripts/surveyApp/mood.viewmodel.js",
@@ -29,11 +29,11 @@ namespace Mood
                 "~/Scripts/util/*.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                $"{jsDepRoot}/bootstrap/dist/js/bootstrap.js",
+                $"{jsDepRoot}/bootstrap/bootstrap.js",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                 $"{cssDepRoot}/bootstrap/dist/css/bootstrap.css",
+                 $"{cssDepRoot}/bootstrap/bootstrap.css",
                  "~/Content/Site.css"));
         }
     }
--- Mood/gulpfile.js
+++ Mood/gulpfile.js
@@ -3,26 +3,38 @@
 
 var gulp = require("gulp"),
     filter = require('gulp-filter'),
+    flatten = require('gulp-flatten'),
     gnf = require("gulp-npm-files"),
     rimraf = require('rimraf');
 
 gulp.task("build:js", function () {
     return gulp.src(gnf(), { base: '.' })
         .pipe(filter(['**/*.js']))
+        .pipe(flatten({ newPath: 'vendor', subPath: [1, 2] }))
         .pipe(gulp.dest("Scripts"));
 });
 gulp.task("build:css", function () {
     return gulp.src(gnf(), { base: '.' })
-        .pipe(filter(['**/*.css', '**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.svg', '**/*.eot']))
+        .pipe(filter(['**/*.css']))
+        .pipe(flatten({ newPath: 'vendor' }))
         .pipe(gulp.dest("Content"));
 });
-gulp.task("build", ['build:js', 'build:css']);
+gulp.task("build:fonts", function () {
+    return gulp.src(gnf(), { base: '.' })
+        .pipe(filter(['**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.svg', '**/*.eot']))
+        .pipe(flatten())
+        .pipe(gulp.dest("Content/fonts"));
+});
+gulp.task("build", ['build:js', 'build:css', 'build:fonts']);
 
 
 gulp.task("clean:js", function (cb) {
-    rimraf("Scripts/node_modules", cb);
+    rimraf("Scripts/vendor/*", cb);
 });
 gulp.task("clean:css", function (cb) {
-    rimraf("Content/node_modules", cb);
+    rimraf("Content/vendor/*", cb);
 });
-gulp.task("clean", ['clean:js', 'clean:css']);
+gulp.task("clean:fonts", function (cb) {
+    rimraf("Content/fonts/*", cb);
+});
+gulp.task("clean", ['clean:js', 'clean:css', 'clean:fonts']);
