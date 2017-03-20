06fc52ca35a058fdf94fefd6a43eecff35a2d3e3
Tyler Evert <tevert2@gmail.com>
2017-03-20 11:07:06 -0500

    Need to start from UTC before local conversion makes sense

--- Mood/Scripts/resultsApp/answer.viewmodel.js
+++ Mood/Scripts/resultsApp/answer.viewmodel.js
@@ -5,7 +5,7 @@
     self.moodId = ko.observable(data.MoodId);
     self.moodName = ko.observable(data.Mood.Description);
     self.details = ko.observable(data.Details);
-    self.time = ko.observable(moment(data.Time).local());
+    self.time = ko.observable(moment.utc(data.Time).local());
     self.timeString = ko.computed(function () { return self.time().format('LLLL') });
     
     return self;
